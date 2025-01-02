bfeefedbabb860a001f3a824f359a6bb3a965f7f
David Thorson <david.thorson@sailpoint.com>
2025-01-02 12:10:58 -0600

    update

--- Python_Project_1.py
+++ Python_Project_1.py
@@ -2,4 +2,5 @@ print("For" + " da" + " Love" + " of" + " da" + " game")
 
 name = input("Add a number here: ")
 
+# This will print the total number of characters in the name variable
 print(len(name))
