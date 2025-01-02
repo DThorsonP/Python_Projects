3e27aa5ac3df29c9fc18d75cb8d06afe7a80e26d
David Thorson <david.thorson@sailpoint.com>
2025-01-02 12:23:15 -0600

    update

---
+++ Python_Project_Beginner.py
@@ -0,0 +1,11 @@
+print("For" + " da" + " Love" + " of" + " da" + " game")
+
+name = input("Add a number here: ")
+
+# This will print the total number of characters in the name variable
+print(len(name))
+
+username = "David"
+length = len(username)
+
+print(length)
