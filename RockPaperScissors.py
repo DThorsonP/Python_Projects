edbfa5332f91e61a6af5bd7cc8f6108893c8cd44
David Thorson <david.thorson@sailpoint.com>
2025-01-03 16:31:40 -0600

    update

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,8 +1,22 @@
 import random
 
-heads_or_tails = random.randint(0, 1)
-if heads_or_tails == 0:
-    print("Heads")
-else:
-    print("Tails")
+options = ["rock", "paper", "scissors"]
 
+user_choice = input("What do you choose? rock, paper or roscissors.\n")
+
+computer_choice = random.choice(options)
+
+print("You chose: ", user_choice)
+
+print("Computer chose: ", computer_choice)
+
+if user_choice == computer_choice:
+    print("It's a draw")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("You win!")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("You win!")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("You win!")
+else:
+    print("Computer wins!")
