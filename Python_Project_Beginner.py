97f18641be75f0e9b7a4a895d6c205ce6161b58c
David Thorson <david.thorson@sailpoint.com>
2025-01-03 12:09:28 -0600

    update

--- Python_Project_Beginner.py
+++ Python_Project_Beginner.py
@@ -1,13 +1,15 @@
-print("Welcome to the tip calculator!")
+print("Welcome")
+height = int(input("What is your height? "))
 
-bill = float(input("What was the total bill? "))
-tip = int(input("How much tip would you like to give? 10, 12, 15 "))
-split = int(input("How many people to split the bill? "))
+if height >= 4:
+    print("You can ride!")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        print("Pay $5")
+    elif age <= 18:
+        print("Pay $7")
+    else:
+        print("Pay $12")
 
-tippercent = tip / 100
-totaltip = bill * tippercent
-total_bill = bill + totaltip
-bill_per_person = total_bill / split
-final_amount = round(bill_per_person, 2)
-
-print(f"Each person should pay: ${final_amount} ")
+else:
+    print("Sorry you cannot ride")
