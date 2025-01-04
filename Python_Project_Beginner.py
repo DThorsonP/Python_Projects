3083a9b30c7874b7c6dac8df242aabd3584c6c9e
David Thorson <david.thorson@sailpoint.com>
2025-01-04 12:51:11 -0600

    update

--- Python_Project_Beginner.py
+++ Python_Project_Beginner.py
@@ -2,6 +2,7 @@ print("Welcome")
 height = int(input("What is your height? "))
 bill = 0
 
+
 if height >= 4:
     print("You can ride!")
     age = int(input("What is your age? "))
@@ -14,12 +15,12 @@ if height >= 4:
     else:
         bill =12
         print("Adult ticket is $12")
-
+    
     want_photo = input("Do you want a photo Y or N? ")
     if want_photo == "Y":
         bill += 3
 
     print(f"Your final bill is {bill}")
-    
+
 else:
     print("Sorry you cannot ride")
