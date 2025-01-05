2d2ffe61c39848ebb896c9ecf24b883d3a250b14
David Thorson <david.thorson@sailpoint.com>
2025-01-05 14:49:16 -0600

    update

--- PasswordGenerator.py
+++
@@ -1,8 +0,0 @@
-numbers = list(range(0, 10))
-letterslower = list(map(chr, range(ord('a'), ord('z') + 1)))
-lettersupper = list(map(chr, range(ord('A'), ord('Z') + 1)))
-
-# print(numbers)
-# print(letterslower)
-# print(lettersupper)
-
