115ae629106d6d710a6091a9ecf968e6cbc9b523
David Thorson <david.thorson@sailpoint.com>
2025-01-12 10:46:14 -0600

    update

--- Beginner_Projects/Dictionaries.py
+++ Beginner_Projects/Dictionaries.py
@@ -1,3 +1,5 @@
+import os
+
 logo = '''
   _______ _             _____ _ _            _                        _   _             
  |__   __| |           / ____(_) |          | |       /\             | | (_)            
@@ -20,10 +22,10 @@ logo = '''
 '''
 print(logo)
 
-bids = {}
-continue_bidding = True
+bids = {} # see line 47
+continue_bidding = True # see line 44 this needs to be defined beforehand or there will be a NameError: name 'continue_bidding' is not defined
 
-def find_highest_bidder(bidding_dictionary):
+def find_highest_bidder(bidding_dictionary): # here bids is the arguement being passed into the bidding_dictionary parameter
     winner = ""
     highest_bid = 0
     for bidder in bidding_dictionary:
@@ -33,14 +35,19 @@ def find_highest_bidder(bidding_dictionary):
             winner = bidder
 
     print(f"The winner is {winner} with a bid of ${highest_bid}")
+    print(bids)
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(logo)
 
 while continue_bidding:
     name = input("What is your name?: ")
     price = int(input("What is your bid?: $"))
-    bids[name] = price # Saves data into dictionary in this format {name: price}
+    bids[name] = price # Saves data into dictionary in this format {name: price} with name being the key and price being the value
     should_continue = input("Are there any other bidders? Type 'yes' or 'no' \n").lower()
     if should_continue == "no":
         continue_bidding = False
         find_highest_bidder(bids)
     elif should_continue == "yes":
-        print("\n" * 100)
+        clear_screen()
