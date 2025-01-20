a56a179e0d357ed1af70997f8e591facb1fd1745
david-thorson-sp <david.thorson@sailpoint.com>
2025-01-20 16:15:39 -0600

    update

--- Intermediate_Projects/Pong/ball.py
+++ Intermediate_Projects/Pong/ball.py
@@ -6,6 +6,7 @@ class Ball(Turtle):
         self.color("white")
         self.shape("square")
         self.penup()
+        self.move_speed = 0.1
         self.x_move = 10
         self.y_move = 10
 
@@ -19,6 +20,7 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def reset_position(self):
         self.goto(0, 0)
