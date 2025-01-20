dfc1e3dba22fbda130865f9e9bf1030681cfd8be
david-thorson-sp <david.thorson@sailpoint.com>
2025-01-20 15:27:58 -0600

    update

--- Intermediate_Projects/Pong/main.py
+++ Intermediate_Projects/Pong/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen = Screen()
 screen.listen()
@@ -32,5 +34,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 325 or ball.distance(l_paddle) < 50 and ball.xcor() < -325:
         ball.bounce_x()
 
+    # detect when r_paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
