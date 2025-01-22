77d46827b9dbce6da701e1cd45a4ba0ac450cdf8
david-thorson-sp <david.thorson@sailpoint.com>
2025-01-22 14:35:08 -0600

    update pandas weather project

--- WeatherData/main.py
+++ WeatherData/main.py
@@ -1,6 +1,8 @@
 #! /Users/david.thorson/.local/pipx/venvs/pandas/bin/python
 
-# import csv
+import csv
+import pandas as pd
+import numpy as np
 
 # with open("weather_data.csv") as data_file:
 #     data = csv.reader(data_file)
@@ -8,9 +10,14 @@
 #     for row in data:
 #         if row[1] != "temp":
 #             temperatures.append(int(row[1]))
-
 #     print(temperatures)
 
-import pandas as pd
-import numpy as np
+data = pd.read_csv("weather_data.csv")
+# print(data["temp"])
+# print(type(data))
+# print(type(data["temp"]))
 
+temp_list = data["temp"].to_list()
+print(temp_list)
+print(data["temp"].mean())
+print(round(data["temp"].mean(), 2))
