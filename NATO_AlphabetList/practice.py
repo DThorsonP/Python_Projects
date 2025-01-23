8e979081705392dfb04ac40bd26a4102e715f147
David Thorson <dthorson700@gmail.com>
2025-01-23 12:22:10 -0600

    update projects

--- NATO_AlphabetList/practice.py
+++ NATO_AlphabetList/practice.py
@@ -1,12 +1,12 @@
-with open("file1.txt") as file1:
-  list1 = file1.readlines()
+# with open("file1.txt") as file1:
+#   list1 = file1.readlines()
     
-with open("file2.txt") as file2:
-  list2 = file2.readlines()
+# with open("file2.txt") as file2:
+#   list2 = file2.readlines()
     
-result = [int(num) for num in list1 if num in list2]
+# result = [int(num) for num in list1 if num in list2]
  
-print(result)
+# print(result)
 
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
