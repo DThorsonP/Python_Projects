85364c458e2d0f01f86a9f539dec8174e05d17eb
David Thorson <dthorson700@gmail.com>
2025-01-28 14:32:13 -0600

    update password manager app

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
 import string
+import json
 
 #Password Generator Project
 def generate_password():
@@ -27,21 +28,34 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                      f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+       
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # UI Setup
-
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -74,5 +88,7 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
+
 
 window.mainloop()
--- PasswordManager/test.py
+++ PasswordManager/test.py
@@ -1,7 +1,43 @@
-import string
-letters = list(string.ascii_letters)
-print(letters)
+try:
+    file = open("noneFile.txt")
+    a_dict = {"key": "value"}
+    print(a_dict["key"])
+except FileNotFoundError:
+    file = open("noneFile.txt", "w")
+    file.write("1234")
+except NameError as error_message:
+    print(f"The key {error_message} does not exist")
+    
 
-# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+fruits = ["Apple", "Pear", "Orange"]
+
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(f"{fruit} pie")
+
+make_pie(4)
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+def count_likes(posts):
+
+    total_likes = 0
+    for post in posts:
+        try:
+            total_likes = total_likes + post['Likes']
+        except KeyError:
+            pass
+    return total_likes
+        
+count_likes(facebook_posts)
