82c1540d231bf260583617002a49fea186c5e385
David Thorson <dthorson700@gmail.com>
2025-02-05 17:09:55 -0600

    updated weather app

--- Intermediate+_Projects/WeatherApp/main.py
+++ Intermediate+_Projects/WeatherApp/main.py
@@ -1,13 +1,14 @@
 import requests
-# Test API call for Open Weather API key
-# https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=06bceed6ec53871ab630e28a834b0ace
+from twilio.rest import Client
 
+account_sid = "AC2791a27f0ee304a8662a03ab0e3f961f"
+auth_token = "87059bbab84fb1ec1d21568e9b2c9ec1"
 OWN_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "06bceed6ec53871ab630e28a834b0ace"
 
 weather_params = {
-    "lat": 30.267153,
-    "lon": -97.743057,
+    "lat": 38.252666,
+    "lon": -85.758453,
     "cnt": 4,
     "appid": api_key,
 }
@@ -15,13 +16,19 @@ weather_params = {
 response = requests.get(OWN_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-# print(weather_data["list"][0]["weather"][0]["id"])
 
 will_rain = False
 for hour_data in weather_data["list"]:
-    condition_codce = hour_data["weather"][0]["id"]
+    condition_code = hour_data["weather"][0]["id"]
     if int(condition_code) < 700:
        will_rain = True
 if will_rain:
-    print("Bring an ☂️")
-    
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        messaging_service_sid='MGbe67c256cea3edf946a583d54bbb6ba9',
+        to="whatsapp:+18887776409",
+        from_="whatsapp:+14155238886",
+        body="It's going to rain so bring an umbrella ☔️ ",
+
+    )
+    print(message.status)
