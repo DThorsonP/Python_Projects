2fb2a88c5f2b32cbed8c8374d84f9a664a51b86d
David Thorson <dthorson700@gmail.com>
2025-02-07 17:06:21 -0600

    update habit tracker app

--- Intermediate+_Projects/HabbitTracker/main.py
+++ Intermediate+_Projects/HabbitTracker/main.py
@@ -1,11 +1,15 @@
 import requests
+from datetime import datetime
 
 # My first graph, woo!
 # https://pixe.la/v1/users/dthorson2700/graphs/graph1.html
 
 USERNAME = "dthorson2700"
 TOKEN = "Y`=(-Ay[MS+0p7.r6AuQ*o=;VBU-c15,T++$IW0^~i)H"
+GRAPH_ID = "graph1"
+
 pixela_endpoint = "https://pixe.la/v1/users"
+
 
 user_params = {
     "token": TOKEN,
@@ -20,7 +24,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Cycling Graph",
     "unit": "km",
     "type": "float",
@@ -30,9 +34,25 @@ graph_config = {
 headers = {
     "X-USER-TOKEN": TOKEN
 }
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response.text)
-
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
 
 # date format yyyyMMdd
 
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime(year=2025, month=2, day=8)
+# print(today.strftime("%Y%m%d"))
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "57.47",
+}
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
